0155fe5524daf9b3aa5a2a8971040c8bc54a25c7
Dmitry <dimonbyt@gmail.com>
2024-02-03 02:25:29 +0300

    add first panel

--- PaintingGuesser/MainWindow.xaml.cs
+++ PaintingGuesser/MainWindow.xaml.cs
@@ -17,9 +17,12 @@ namespace PaintingGuesser
 {
     public partial class MainWindow : Window
     {
+        int total = 0;
+        int correct = 0;
         public MainWindow()
         {
             InitializeComponent();
+            textBlock.Text = string.Empty;
         }
 
         private void DragArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -27,19 +30,29 @@ namespace PaintingGuesser
             this.DragMove();
         }
 
+        private void DisableCheckBox(object sender)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                Grid parentGrid = button.Parent as Grid;
+                if (parentGrid != null)
+                {
+                    foreach (var child in parentGrid.Children)
+                    {
+                        if (child is CheckBox checkBox)
+                        {
+                            checkBox.IsChecked = false;
+                        }
+                        
+                    }
+                }
+            }
+        }
+
         private void buttonExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
-        }
-
-        private void checkStart_Checked(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine("Checkbox is checked.");
-        }
-
-        private void checkStart_Unchecked(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine("Checkbox is unchecked.");
         }
 
         private void buttonStart_Click(object sender, RoutedEventArgs e)
@@ -47,5 +60,92 @@ namespace PaintingGuesser
             gridStart.Visibility = Visibility.Hidden;
             grid1.Visibility = Visibility.Visible;
         }
+
+        private void Correct_Select(object sender, RoutedEventArgs e)
+        {
+            if (total >= 4)
+            {
+                CheckBox checkBox = sender as CheckBox;
+                if (checkBox != null)
+                {
+                    MessageBox.Show("Вы выбрали максимум пунктов!", "Painting Guesser");
+                    checkBox.IsChecked = false;
+                    total++;
+                    return;
+                }
+            }
+
+            correct++;
+            total++;
+        }
+
+        private void Select(object sender, RoutedEventArgs e)
+        {
+            if (total >= 4)
+            {
+                CheckBox checkBox = sender as CheckBox;
+                if (checkBox != null)
+                {
+                    MessageBox.Show("Вы выбрали максимум пунктов!", "Painting Guesser");
+                    checkBox.IsChecked = false;
+                    total++;
+                    return;
+                }
+            }
+
+            total++;
+        }
+        private void Unselect(object sender, RoutedEventArgs e)
+        {
+            total--;
+        }
+
+        private void Correct_Unselect(object sender, RoutedEventArgs e)
+        {
+            total--;
+            correct--;
+        }
+
+        private void ButtonCheck1_Click(object sender, RoutedEventArgs e)
+        {
+            if(correct >= 2)
+            {
+                DisableCheckBox(sender);
+                (sender as Button).IsEnabled = false;
+
+                textBlock.TextAlignment = TextAlignment.Justify;
+                textBlock.Text = "Образование – первая потребность человека после хлеба насущного (Дантон)";
+
+                buttonNext1.Content = "Дальше";
+                buttonNext1.IsEnabled = true;
+
+                total = 0;
+                correct = 0;
+
+                return;
+            }
+
+            else if(total == 0)
+            {
+                MessageBox.Show("Вы ничего не выбрали.", "Painting Guesser");
+
+                return;
+            }
+
+            else
+            {
+                DisableCheckBox(sender);
+
+                textBlock.TextAlignment = TextAlignment.Center;
+                textBlock.Text = "Увы, вы не отгадали ассоциации, можете попробовать еще раз.";
+
+                total = 0;
+                correct = 0;
+
+                return;
+            }
+        }
+
+
     }
 }
